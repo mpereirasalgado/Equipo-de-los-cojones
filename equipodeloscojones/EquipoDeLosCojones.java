46798e3b788ab43d14c2dcd87a58b20ca912fd18
dseijasiglesias <dseijasiglesias@eniac08.danielcastelao.org>
2014-12-01 10:35:39 +0100

    Segundo Commit

--- equipodeloscojones/EquipoDeLosCojones.java
+++ equipodeloscojones/EquipoDeLosCojones.java
@@ -6,6 +6,7 @@ public class EquipoDeLosCojones {
     public static void main(String[] args) 
     {
         System.out.println("mierda de equipo");
+        System.out.println("Seijas es el mejor");
     }
     
 }
