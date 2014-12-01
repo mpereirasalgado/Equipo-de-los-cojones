a538769a5731fbc06b1952f60737bfd27e1e8711
mpereirasalgado <mpereirasalgado@eniac07.danielcastelao.org>
2014-12-01 10:04:30 +0100

    primer cambio

---
+++ equipodeloscojones/EquipoDeLosCojones.java
@@ -0,0 +1,11 @@
+package equipodeloscojones;
+
+/**@author mpereirasalgado*/
+public class EquipoDeLosCojones {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("mierda de equipo");
+    }
+    
+}
